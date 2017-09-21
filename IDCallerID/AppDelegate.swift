457f3b999b65a2a6424852324b0ca88fefb7f17b
Kenneth Wieschhoff <kenneth.wieschhoff@sbdinc.com>
2017-09-21 10:18:07 -0400

    Remove unneeded files

---
+++ IDCallerID/AppDelegate.swift
@@ -0,0 +1,82 @@
+//
+//  AppDelegate.swift
+//  IDCallerID
+//
+//  Created by Kenneth Wieschhoff on 9/19/17.
+//  Copyright © 2017 Stanley Black&Decker. All rights reserved.
+//
+
+import UIKit
+import ContactsUI
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var allContacts : [CNContact] = []
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        fetchContacts()
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func fetchContacts()
+    {
+        let contactStore = CNContactStore()
+        var allContainers : [CNContainer] = []
+        
+        contactStore.requestAccess(for: .contacts) { (sucess, error) in
+            //you can use one of these/ all keys to filter contacts
+            let keysToFetch = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactOrganizationNameKey,   CNContactJobTitleKey, CNContactPhoneNumbersKey, CNContactIdentifierKey]
+            do{
+                // _______________ Fetch all the Containers_________________________________
+                allContainers = try contactStore.containers(matching: nil)
+                
+            }
+            catch{
+                print(error)
+            }
+            for container in allContainers{
+                let fetchPredicate = CNContact.predicateForContactsInContainer(withIdentifier: container.identifier)
+                do{
+                    //____________Fetch all the contacts corresponding to every Container______
+                    let containerResults = try contactStore.unifiedContacts(matching: fetchPredicate, keysToFetch: keysToFetch as [CNKeyDescriptor])
+                    self.allContacts.append(contentsOf: containerResults)
+                    NotificationCenter.default.post(name: Notification.Name("Reload"), object: nil)
+
+                }
+                catch{
+                    print(error)
+                }
+            }
+        }
+    }
+
+}
+
---
+++ IDCallerID/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  IDCallerID
+//
+//  Created by Kenneth Wieschhoff on 9/19/17.
+//  Copyright © 2017 Stanley Black&Decker. All rights reserved.
+//
+
+import UIKit
+import ContactsUI
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    let appd = UIApplication.shared.delegate as! AppDelegate
+    @IBOutlet weak var tableView: UITableView!
+    
+    var filteredContacts : [CNContact] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadData), name: NSNotification.Name(rawValue: "Reload"), object: nil)
+
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 55.0
+        
+        self.tableView.register(UINib(nibName: "ContactTableViewCell", bundle:nil),
+                                forCellReuseIdentifier: "ContactTableViewCell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @objc func reloadData() {
+        DispatchQueue.main.async {
+            self.filteredContacts = self.appd.allContacts
+            self.tableView.reloadData()
+        }
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredContacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: ContactTableViewCell?
+        cell = tableView.dequeueReusableCell(withIdentifier: "ContactTableViewCell") as? ContactTableViewCell
+        
+        if cell == nil {
+            cell = UINib(nibName: "ContactTableViewCell", bundle:nil) as? ContactTableViewCell
+            cell = ContactTableViewCell()
+        }
+        
+        let contact = filteredContacts[indexPath.row]
+        cell?.name.text = contact.givenName + " " + contact.familyName
+        var numbers: String = ""
+        for phoneNumber in contact.phoneNumbers {
+            numbers +=  phoneNumber.value.stringValue + "\n"
+        }
+        cell?.number.text = numbers
+        return cell!
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredContacts = appd.allContacts
+        } else {
+            filteredContacts = appd.allContacts.filter({ (contact) -> Bool in
+                for phone in contact.phoneNumbers {
+                    let phoneNumber = phone.value.stringValue.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+                    if phoneNumber.contains(searchText) {
+                        return true
+                    }
+                }
+                return false
+            })
+        }
+        tableView.reloadData()
+    }
+}
+
+class ContactTableViewCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var number: UILabel!
+}
